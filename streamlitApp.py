f2a3d63de4ab6f4ae4bc21a10ee6bba71c1ffcfc
Takuma Watari <104181103+takumawatari@users.noreply.github.com>
2025-02-24 17:01:44 +0100

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -21,7 +21,6 @@ country = st.selectbox('**Select country:**', country_list)
 sankey_file_path = f"sankey/{country}_{year}.svg"
 
 # open
-#st.image(sankey_file_path)
 with open(sankey_file_path, encoding="utf8") as file:
     svg_content = file.read()
 st.markdown(f'<div style="justify-content: center;">{svg_content}</div>', unsafe_allow_html=True)

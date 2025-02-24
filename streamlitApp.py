af8d0e4e1c0ee05c459800c31e3a480f017adef7
Takuma Watari <104181103+takumawatari@users.noreply.github.com>
2025-02-24 16:36:38 +0100

    update

--- streamlitApp.py
+++ streamlitApp.py
@@ -10,11 +10,11 @@ st.markdown("**Author**: [Takuma Watari](https://takuma-watari.com/en/) (Nationa
 year = [2019]
 file_path = os.path.join('data', f'data_{year}.xlsx')
 country_list = ['Argentina','Chile','Colombia','Ecuador','Peru',
-               'Algeria','Angola','Ghana','Kenya','Morocco','Nigeria','Tanzania',
-               'Malaysia','Philippines','Thailand','Vietnam']
+                'Algeria','Angola','Ghana','Kenya','Morocco','Nigeria','Tanzania',
+                'Malaysia','Philippines','Thailand','Vietnam']
 country = st.selectbox('Select country', country_list)
 
 sankey_file_path = f"sankey/{country}_{year}.svg"
 with open(sankey_file_path, encoding="utf8") as file:
-        svg_content = file.read()
-    st.markdown(f'<div style="display: flex; justify-content: center;">{svg_content}</div>', unsafe_allow_html=True)
+    svg_content = file.read()
+st.markdown(f'<div style="display: flex; justify-content: center;">{svg_content}</div>', unsafe_allow_html=True)
